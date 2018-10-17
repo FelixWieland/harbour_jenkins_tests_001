76ad6ddba1b4140afbf12dcaff92b79832a83bdb
FW <felix.wieland@mail.de>
2018-10-17 08:13:30 +0200

    Jenkins Test 12

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("<h1>Hello Jenkins! It Worked again!</h1>");
+    response.end("<h1>Hello Jenkins! It Worked again! Demo!</h1>");
 });
 
 // listen on localhost:8000
